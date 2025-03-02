fa0863f0407566fda63ab7e6ef679e4dd18c8095
Udean777 <uaart123@gmail.com>
2025-03-02 22:31:20 +0700

    FEAT: Adding custom bottom tab bar, and adding few pages in it.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,21 +1,20 @@
 import { Tabs } from "expo-router";
 import React from "react";
 import * as Icons from "phosphor-react-native";
+import CustomTabBar from "@/components/CustomTabBar";
 
 export default function TabLayout() {
   return (
     <Tabs
+      tabBar={CustomTabBar}
       screenOptions={{
         headerShown: false,
       }}
     >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => <Icons.House size={28} color={color} />,
-        }}
-      />
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="statistics" />
+      <Tabs.Screen name="wallet" />
+      <Tabs.Screen name="profile" />
     </Tabs>
   );
 }
