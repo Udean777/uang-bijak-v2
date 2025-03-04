821ab4e7ad875e112c19b63451a6ae249ffcd09e
Udean777 <uaart123@gmail.com>
2025-03-05 03:44:57 +0700

    FEAT: Designing statistics screen, and implementing showing chart for weekly, monthly and yearly.

--- app/(tabs)/statistics.tsx
+++ app/(tabs)/statistics.tsx
@@ -1,14 +1,189 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Alert, ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { colors, radius, spacingX, spacingY } from "@/constants/Colors";
+import { scale, verticalScale } from "@/utils/style";
+import { useAuth } from "@/context/authContext";
+import {
+  fetchMonthlyStats,
+  fetchWeeklyStats,
+  fetchYearlyStats,
+} from "@/services/transactionService";
+import ScreenWrapper from "@/components/ScreenWrapper";
+import CustomHeader from "@/components/CustomHeader";
+import SegmentedControl from "@react-native-segmented-control/segmented-control";
+import Loading from "@/components/Loading";
+import { BarChart } from "react-native-gifted-charts";
+import TransactionList from "@/components/TransactionList";
 
 const Page = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [chartData, setChartData] = useState([]);
+  const [chartLoading, setChartLoading] = useState(false);
+  const [transactions, setTransactions] = useState([]);
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (activeIndex == 0) {
+      getWeeklyStats();
+    }
+    if (activeIndex == 1) {
+      getMonthlyStats();
+    }
+    if (activeIndex == 2) {
+      getYearlyStats();
+    }
+  }, [activeIndex]);
+
+  const getWeeklyStats = async () => {
+    setChartLoading(true);
+    let res = await fetchWeeklyStats(user?.uid as string);
+    setChartLoading(false);
+
+    if (res.success) {
+      setChartData(res.data.stats);
+      setTransactions(res.data.transactions);
+    } else {
+      Alert.alert("Error", res.msg);
+    }
+  };
+
+  const getMonthlyStats = async () => {
+    setChartLoading(true);
+    let res = await fetchMonthlyStats(user?.uid as string);
+    setChartLoading(false);
+
+    if (res.success) {
+      setChartData(res.data.stats);
+      setTransactions(res.data.transactions);
+    } else {
+      Alert.alert("Error", res.msg);
+    }
+  };
+
+  const getYearlyStats = async () => {
+    setChartLoading(true);
+    let res = await fetchYearlyStats(user?.uid as string);
+    setChartLoading(false);
+
+    if (res.success) {
+      setChartData(res.data.stats);
+      setTransactions(res.data.transactions);
+    } else {
+      Alert.alert("Error", res.msg);
+    }
+  };
+
   return (
-    <View>
-      <Text>Statistics</Text>
-    </View>
+    <ScreenWrapper>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <CustomHeader title="Statistics" />
+        </View>
+
+        <ScrollView
+          contentContainerStyle={{
+            gap: spacingY._20,
+            paddingTop: spacingY._5,
+            paddingBottom: verticalScale(100),
+          }}
+          showsVerticalScrollIndicator={false}
+        >
+          <SegmentedControl
+            values={["Mingguan", "Bulanan", "Tahunan"]}
+            selectedIndex={activeIndex}
+            onChange={(event) => {
+              setActiveIndex(event.nativeEvent.selectedSegmentIndex);
+            }}
+            tintColor={colors.primary}
+            backgroundColor={colors.neutral200}
+            appearance="light"
+            activeFontStyle={styles.segmentFontStyle}
+            style={styles.segmentStyle}
+            fontStyle={{ ...styles.segmentFontStyle, color: colors.neutral900 }}
+          />
+
+          <View style={styles.chartContainer}>
+            {chartData.length > 0 ? (
+              <BarChart
+                data={chartData}
+                barWidth={scale(30)}
+                spacing={[1, 2].includes(activeIndex) ? scale(25) : scale(16)}
+                // roundedTop
+                // roundedBottom
+                hideRules
+                yAxisThickness={0}
+                xAxisThickness={0}
+                // hideYAxisText
+                xAxisLabelTextStyle={{
+                  color: colors.neutral600,
+                  fontSize: verticalScale(10),
+                }}
+                noOfSections={5}
+                minHeight={5}
+                barBorderRadius={10}
+              />
+            ) : (
+              <View style={styles.noChart} />
+            )}
+
+            {chartLoading && (
+              <View style={styles.chartLoadingContainer}>
+                <Loading color={colors.neutral900} />
+              </View>
+            )}
+          </View>
+
+          <TransactionList
+            title="Transaksi"
+            emptyListMessage="Tidak ada transaksi yang ditemukan"
+            data={transactions}
+          />
+        </ScrollView>
+      </View>
+    </ScreenWrapper>
   );
 };
 
 export default Page;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  chartContainer: {
+    position: "relative",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  chartLoadingContainer: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    borderRadius: radius._12,
+    backgroundColor: "rgba(255, 255, 255, 0.6)",
+  },
+  header: {},
+  noChart: {
+    backgroundColor: "rgba(255, 255, 255, 0.6)",
+    height: verticalScale(210),
+  },
+  searchIcon: {
+    backgroundColor: colors.neutral300,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 100,
+    height: verticalScale(35),
+    width: verticalScale(35),
+    borderCurve: "continuous",
+  },
+  segmentStyle: {
+    height: scale(37),
+  },
+  segmentFontStyle: {
+    fontSize: verticalScale(11),
+    fontWeight: "bold",
+    color: colors.white,
+  },
+  container: {
+    paddingHorizontal: spacingX._20,
+    paddingVertical: spacingY._5,
+    gap: spacingY._10,
+  },
+});
